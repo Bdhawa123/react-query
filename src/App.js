846468a61f595e3d3cf7c55ed25f88a0ff62b0ca
Binay Dhawa <binaydhawa@outlook.com>
2021-08-30 14:27:22 +1000

    react-query variables

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from "./Components/Navbar";
 import People from "./Components/People";
 import Planets from "./Components/Planets";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState("planets");
 
   return (
-    <div className="App">
-      <h1>Star wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
-      </div>
-    </div>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Star wars Info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === "planets" ? <Planets /> : <People />}
+          </div>
+        </div>
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/Components/Planets.js
+++ src/Components/Planets.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async (pageNumb) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${pageNumb}`);
+  return res.json();
+};
 
 const Planets = () => {
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planets", page], () =>
+    fetchPlanets(page)
+  );
+
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
+
+      {status === "loading" && <div>Loading data ... </div>}
+      {status === "error" && <div>Error fetching data </div>}
+
+      {status === "success" && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
